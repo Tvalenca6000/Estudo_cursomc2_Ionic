9e30ce19387763780c86c43eebb1e1b246e15d8b
Tvalenca6000 <Tvalenca6000@gmail.com>
2022-08-22 10:29:56 -0300

    Resources & Services | Paginação com parâmetros

--- src/main/java/com/tvalenca/cursomc/resources/Categoria_Resource.java
+++ src/main/java/com/tvalenca/cursomc/resources/Categoria_Resource.java
@@ -5,11 +5,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -58,4 +60,16 @@ public class Categoria_Resource {
         return ResponseEntity.ok().body(listDto);
     }
     
+    @RequestMapping(value="/page", method=RequestMethod.GET)
+    public ResponseEntity<Page<?>> findPage(
+            @RequestParam(value = "page",defaultValue = "0") Integer page, 
+            @RequestParam(value = "linesPerPage",defaultValue = "24") Integer linesPerPage, //DefaultValue 24 pois ele é multiplo de 1,2,3,4. Fica mais facil de organizar o layout responsivo
+            @RequestParam(value = "orderBy",defaultValue = "nome") String orderBy, 
+            @RequestParam(value = "direction",defaultValue = "ASC") String direction) {
+
+        Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+        Page<Categoria_DTO> listDto = list.map(obj -> new Categoria_DTO(obj));
+        return ResponseEntity.ok().body(listDto);
+    }
+
 }
