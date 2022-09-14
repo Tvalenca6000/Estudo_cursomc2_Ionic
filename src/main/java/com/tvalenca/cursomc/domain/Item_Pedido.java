f15b0ffc22c4095f432e5908cef1c011052990fa
Tvalenca6000 <Tvalenca6000@gmail.com>
2022-09-14 11:21:39 -0300

    Domain | Item_Pedido Set Pedido e Produto

--- src/main/java/com/tvalenca/cursomc/domain/Item_Pedido.java
+++ src/main/java/com/tvalenca/cursomc/domain/Item_Pedido.java
@@ -38,8 +38,16 @@ public class Item_Pedido implements Serializable {
         return id.getPedido();
     }
 
+    public void setPedido(Pedido pedido){
+        id.setPedido(pedido);
+    }
+
     public Produto getProduto(){
         return id.getProduto();
+    }
+
+    public void setProduto(Produto produto){
+        id.setProduto(produto);
     }
 
     public Item_Pedido_PK getId() {
