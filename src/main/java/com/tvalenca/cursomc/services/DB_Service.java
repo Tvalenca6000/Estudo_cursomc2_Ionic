d9ff3b3bbc54868d0eb7f2047170566746358fc9
Tvalenca6000 <Tvalenca6000@gmail.com>
2022-10-13 10:22:46 -0300

    Domain & Services | Perfil ou Perfis do Cliente

--- src/main/java/com/tvalenca/cursomc/services/DB_Service.java
+++ src/main/java/com/tvalenca/cursomc/services/DB_Service.java
@@ -20,6 +20,7 @@ import com.tvalenca.cursomc.domain.Pagamento_Com_Cartao;
 import com.tvalenca.cursomc.domain.Pedido;
 import com.tvalenca.cursomc.domain.Produto;
 import com.tvalenca.cursomc.domain.enums.Estado_Pagamento;
+import com.tvalenca.cursomc.domain.enums.Perfil;
 import com.tvalenca.cursomc.domain.enums.Tipo_Cliente;
 import com.tvalenca.cursomc.repositories.Categoria_Repository;
 import com.tvalenca.cursomc.repositories.Cidade_Repository;
@@ -118,13 +119,20 @@ public class DB_Service {
 
 		cli1.getTelefones().addAll(Arrays.asList("27363323","93838393"));
 
+		Cliente cli2 = new Cliente(null, "Ana Costa", "nelio.iftm@gmail.com", "31628382740", Tipo_Cliente.PESSOAFISICA, pe.encode("123"));
+		cli2.addPerfil(Perfil.ADMIN);
+
+		cli2.getTelefones().addAll(Arrays.asList("57363999","23838111"));
+
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		Endereco e3 = new Endereco(null, "Avenida Floriano", "2106", null, "Centro", "28573212", cli2, c2);
 
 		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1,e2));
+		clienteRepository.saveAll(Arrays.asList(cli1,cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1,e2,e3));
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy hh:mm");
 
