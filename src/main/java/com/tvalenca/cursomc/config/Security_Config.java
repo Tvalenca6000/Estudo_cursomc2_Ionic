f73e407854b89dbe827a3bb2e117bfcfbceecbab
Tvalenca6000 <Tvalenca6000@gmail.com>
2022-10-19 10:47:17 -0300

    Config & Security | Implementando Autorização

--- src/main/java/com/tvalenca/cursomc/config/Security_Config.java
+++ src/main/java/com/tvalenca/cursomc/config/Security_Config.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.tvalenca.cursomc.security.JWTAuthenticationFilter;
+import com.tvalenca.cursomc.security.JWTAuthorizationFilter;
 import com.tvalenca.cursomc.security.JWTUtil;
 
 @Configuration
@@ -57,6 +58,7 @@ public class Security_Config extends WebSecurityConfigurerAdapter{
             .antMatchers(PUBLIC_MATCHERS).permitAll()
             .anyRequest().authenticated();
         http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+        http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
