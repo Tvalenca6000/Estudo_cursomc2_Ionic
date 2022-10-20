28edbb847f4a900c1f6a18ab4602ef4fbf5acc4e
Tvalenca6000 <Tvalenca6000@gmail.com>
2022-10-20 08:42:39 -0300

    Resources | Autorização especifica

--- src/main/java/com/tvalenca/cursomc/resources/Categoria_Resource.java
+++ src/main/java/com/tvalenca/cursomc/resources/Categoria_Resource.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,6 +35,7 @@ public class Categoria_Resource {
         return ResponseEntity.ok().body(obj);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<Void> insert(@Valid @RequestBody Categoria_DTO objDto){
         Categoria obj = service.fromDto(objDto);
@@ -43,6 +45,7 @@ public class Categoria_Resource {
         return ResponseEntity.created(uri).build();
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     public ResponseEntity<Void> update(@Valid @RequestBody Categoria_DTO objDto, @PathVariable Integer id){
         Categoria obj = service.fromDto(objDto);
@@ -57,6 +60,7 @@ public class Categoria_Resource {
         return ResponseEntity.noContent().build();
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @RequestMapping(method=RequestMethod.GET)
     public ResponseEntity<List<Categoria_DTO>> findAll(){
         List<Categoria> list = service.findAll();
