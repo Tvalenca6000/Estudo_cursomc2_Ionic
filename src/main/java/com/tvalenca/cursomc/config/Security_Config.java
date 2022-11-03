fafebe92f65708176ae731bd7635181140be0414
Tvalenca6000 <Tvalenca6000@gmail.com>
2022-11-03 13:52:22 -0300

    Config | Acesso ao esqueci a senha

--- src/main/java/com/tvalenca/cursomc/config/Security_Config.java
+++ src/main/java/com/tvalenca/cursomc/config/Security_Config.java
@@ -47,7 +47,8 @@ public class Security_Config extends WebSecurityConfigurerAdapter{
     };
 
     private static final String[] PUBLIC_MATCHERS_POST = {
-        "/clientes/**"
+        "/clientes/**",
+        "/auth/forgot/**"
     };
 
     @Override
