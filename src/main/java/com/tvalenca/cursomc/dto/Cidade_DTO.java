e0186e5ad5a618551d90e84ce377c1b7c3556e7b
Tvalenca6000 <Tvalenca6000@gmail.com>
2023-03-30 08:45:04 -0300

    Dto | Estado

---
+++ src/main/java/com/tvalenca/cursomc/dto/Cidade_DTO.java
@@ -0,0 +1,37 @@
+package com.tvalenca.cursomc.dto;
+
+import java.io.Serializable;
+
+import com.tvalenca.cursomc.domain.Cidade;
+
+public class Cidade_DTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    private Integer id;
+    private String nome;
+
+    public Cidade_DTO(){}
+
+    public Cidade_DTO(Cidade obj){
+        id = obj.getId();
+        nome = obj.getNome();
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
